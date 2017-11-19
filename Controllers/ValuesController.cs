b577f9037f0762a03279e86e9b5adde14a4b98e4
x00n <burgio@gmail.com>
2017-11-19 10:11:37 +0100

    First commit of BlastCannon App

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blastcannon.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlastController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            var httpConnectionFeature = HttpContext.Features.Get<IHttpConnectionFeature>();
+            var localIpAddress = httpConnectionFeature?.LocalIpAddress;
+            Console.WriteLine("Bang!");
+            return "Bang!" 
+            + localIpAddress 
+            + " @ " 
+            + DateTime.Now.ToString();
+        }
+    }
+}
